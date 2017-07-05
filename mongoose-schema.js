00b0b72f4b9e9cabd58213fd59f23e26718ce7e3
BangYuWen <bangyuwen@gmail.com>
2017-07-05 17:07:03 +0800

    crypt

--- mongoose-schema.js
+++ mongoose-schema.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose'),
       Schema = mongoose.Schema,
       autoIncrement = require('mongoose-auto-increment')
 
-const connection = mongoose.createConnection('mongodb://heroku_q8bbz29v:u2meii8dsu33c3g66scij9g1s5@ds119151.mlab.com:19151/heroku_q8bbz29v')
-
+const connection = mongoose.createConnection(process.env.MONGOLAB_URI)
 autoIncrement.initialize(connection)
 
 const urlSchema = new Schema({
